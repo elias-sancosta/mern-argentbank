cf6ca94323e53066e8a2aec28aed0070c1dc540d
eliasjfscosta <christophe.fs.costa@gmail.com>
2023-08-28 13:46:56 +0200

    Inclusion of comments

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,8 +1,17 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom'; // Importe les composants Navigate et Outlet de react-router-dom
+import { useSelector } from 'react-redux'; // Importe le hook useSelector de react-redux
 
+/**
+ * Composant de route privée.
+ * Vérifie si l'utilisateur est authentifié en utilisant le state de Redux.
+ * Redirige vers l'écran de connexion si l'utilisateur n'est pas authentifié.
+ * @returns {JSX.Element} Élément JSX représentant la route privée.
+ */
 const PrivateRoute = () => {
-  const { userInfo } = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth); // Obtient les informations sur l'utilisateur depuis le state Redux
+
+  // Si l'utilisateur est authentifié, affiche les composants enfants (Outlet)
+  // Sinon, redirige vers l'écran de connexion en remplaçant l'historique de navigation
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 };
 export default PrivateRoute;
